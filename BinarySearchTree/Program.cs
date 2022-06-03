73526fee1eae94bdf9c5c76b16d6860da984f81b
[prasanna] <prasanna>
2022-06-03 15:10:41 +0530

    [prasanna]; adding nodes to a tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,8 +8,18 @@ namespace BinarySearchTree
             BinarySearchTree.BinarySearch<int> tree = new BinarySearchTree.BinarySearch<int>(56);
             tree.AddNode(30);
             tree.AddNode(70);
+            tree.AddNode(22);
+            tree.AddNode(40);
+            tree.AddNode(11);
+            tree.AddNode(3);
+            tree.AddNode(16);
+            tree.AddNode(60);
+            tree.AddNode(95);
+            tree.AddNode(65);
+            tree.AddNode(63);
+            tree.AddNode(67);
+            
             tree.Display();
-
         }
     }
 }
